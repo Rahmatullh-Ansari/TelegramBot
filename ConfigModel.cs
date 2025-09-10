16af417b5eea7db4228f3aab9f107f1d75b6afeb
Rahmatullah_Ansari <rahmatullahansari@globussoft.in>
2025-09-10 16:23:47 +0530

    ++Updated Codes

--- ConfigModel.cs
+++ ConfigModel.cs
@@ -2,8 +2,8 @@
 {
     public class ConfigModel
     {
-        public int YOUR_API_ID { get; set; } = 29072518;//Your account APP ID.
-        public string YOUR_API_HASH { get; set; } = "b0a56e284f6f078494e7d39dbcf12157";//Your account API HASH
-        public string Number { get; set; } = "+917999191216";//Your contact number
+        public int YOUR_API_ID { get; set; }//Your account APP ID.
+        public string YOUR_API_HASH { get; set; }//Your account API HASH
+        public string Number { get; set; }//Your contact number
     }
 }
